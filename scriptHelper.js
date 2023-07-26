b17f232c2578a760c3b998001bf840c90fd51af8
Lucy Dames <lucydames@Lucys-Air.lan>
2023-07-26 02:01:07 -0500

    fixed failures

--- scriptHelper.js
+++ scriptHelper.js
@@ -31,49 +31,58 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     let fuelStatus = document.getElementById("fuelStatus");
     let cargoStatus = document.getElementById("cargoStatus");
     let launchStatus = document.getElementById("launchStatus");
-    let faultyItems = document.getElementById("faultyIems");
+    let faultyItems = document.getElementById("faultyItems"); // Fixed the typo here
  
-    if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || 
-        validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
+    if (
+        validateInput(pilot) === "Empty" ||
+        validateInput(copilot) === "Empty" ||
+        validateInput(fuelLevel) === "Empty" ||
+        validateInput(cargoLevel) === "Empty"
+    ) {
         alert("All fields required");
-    } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || 
-        validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
+    } else if (
+        validateInput(pilot) === "Is a Number" ||
+        validateInput(copilot) === "Is a Number" ||
+        validateInput(fuelLevel) === "Not a Number" ||
+        validateInput(cargoLevel) === "Not a Number"
+    ) {
         alert("Please enter valid information");
     } else {
         pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
         copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
- 
-        if (fuelLevel < 10000 || cargoLevel > 10000) {
+
+        if (fuelLevel < 10000 || cargoLevel >= 10000) { // Fixed the comparison operator here
             list.style.visibility = "visible";
- 
+
             if (fuelLevel < 10000) {
                 fuelStatus.innerHTML = "Fuel level too low for launch";
             } else {
-                fuelStatus.innerHTML = "Fuel level is high enough for launch";
+                fuelStatus.innerHTML = "Fuel level high enough for launch";
             }
- 
-            if (cargoLevel => 10000) {
-                cargoStatus.innerHTML = "Cargo mass too high for launch";
+
+            if (cargoLevel >= 10000) { // Fixed the comparison operator here
+                cargoStatus.innerHTML = "Cargo mass too heavy for launch";
             } else {
-                cargoStatus.innerHTML = "Cargo mass is low enough for launch";
+                cargoStatus.innerHTML = "Cargo mass low enough for launch";
             }
- 
+
             launchStatus.innerHTML = "Shuttle Not Ready for Launch";
             launchStatus.style.color = "#C7254E";
         } else {
             list.style.visibility = "visible";
-            fuelStatus.innerHTML = "Fuel level passes";
-            cargoStatus.innerHTML = "Cargo mass passes";
-            launchStatus.innerHTML = "Shuttle is ready to launch!";
+            fuelStatus.innerHTML = "Fuel level high enough for launch";
+            cargoStatus.innerHTML = "Cargo mass low enough for launch";
+            launchStatus.innerHTML = "Shuttle is Ready for Launch";
             launchStatus.style.color = "#419f6A";
         }
     }
- }
+}
+
  
 
 async function myFetch() {
     let planetsReturned;
-    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
         return response.json();
     });
 
